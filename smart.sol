c12d9cd07f0da7504d06e78fab5c698f50a0a203
eleazartubers <108503804+eleazartubers@users.noreply.github.com>
2024-05-24 21:10:38 +0800

    smart.sol

--- smart.sol
+++ smart.sol
@@ -1,24 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-contract SmartContract {
-    uint256 public num;
+contract SmartContractSavings {
+    uint256 public totalSavings;
 
-    function setNum(uint256 _newNum) external {
-        require(_newNum >= 200 && _newNum <= 1000, "You can only enter up to 200 but cannot be above 1000");
-        num = _newNum;
+    constructor(uint256 _initialSavings) {
+        require(_initialSavings > 0, "Intial Savings Balance must be greater than 0.");
+        totalSavings = _initialSavings;
     }
 
-    function assertNum(uint256 _val) external pure returns (uint256) {
-        assert(_val >= 200 && _val <=1000);
-        return _val;
+    function increaseSavings(uint256 _amount) public {
+        uint256 newTotalSavings = totalSavings + _amount;
+        require(newTotalSavings > totalSavings, "Overflowed!");
+        totalSavings = newTotalSavings;
     }
 
-    function revertVal(uint256 _val) external pure returns (uint256) {
-        if (_val >= 100 && _val <=1000) {
-            revert("Value must between 200 and 1000");
+    function assertInterest(uint256 _val) external pure returns (uint256) {
+        uint256 balance = _val * 2;
+        assert(balance > _val);
+        return balance;
+    }
+
+    function revertInterest(uint256 _val) public pure returns (uint256) {
+        require(_val != 0, "Initial Deposit must not be 0.");
+        if (_val > 5000) {
+            revert("The maximum amount for the account exceeds the limit");
         }
-
-        return _val;
+        return _val * 2;
     }
 }
